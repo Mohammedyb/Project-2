ad5fb69176aa1a694060deac331371aadfbd1f57
connor-n-munro <connor.n.munro@gmail.com>
2022-02-23 15:25:04 -0500

    Fixing up foundational project!

--- ProjectManagementAPI/src/main/java/revature/ProjectManagementAPI/ProjectManagementApiApplication.java
+++ ProjectManagementAPI/src/main/java/revature/ProjectManagementAPI/ProjectManagementApiApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectManagementApiApplication {
+	/**
+	 * I'm just making a change so that we will have a change to push!
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementApiApplication.class, args);
