0626793d24a40d4245f292571665824603e3159d
Mohammed Bubshait <you@example.com>
2022-02-22 22:24:30 -0500

    Added my branch

---
+++ Mohammed_Bubshait/Email2/src/main/java/com/example/email2/Email2Application.java
@@ -0,0 +1,13 @@
+package com.example.email2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Email2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Email2Application.class, args);
+    }
+
+}
