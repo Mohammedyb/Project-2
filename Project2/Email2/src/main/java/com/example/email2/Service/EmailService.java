664e0c5cb62747e08784f72f0944b4f61e30aec9
Mohammed Bubshait <you@example.com>
2022-02-24 10:57:56 -0500

    Trying to add everything

---
+++ Project2/Email2/src/main/java/com/example/email2/Service/EmailService.java
@@ -0,0 +1,59 @@
+package com.example.email2.Service;
+
+import com.example.email2.DTO.EmailDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.mail.SimpleMailMessage;
+        import org.springframework.mail.javamail.JavaMailSender;
+        import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+        import javax.mail.MessagingException;
+        import javax.mail.internet.InternetAddress;
+        import javax.mail.internet.MimeMessage;
+        import java.io.UnsupportedEncodingException;
+
+@Service("mailService")
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public EmailService() {
+    }
+
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendEmail(EmailDTO mail) {
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+
+        try {
+
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+            mimeMessageHelper.setSubject(mail.getSubject());
+            mimeMessageHelper.setFrom(new InternetAddress(mail.getFrom(), "gmail.com"));
+            mimeMessageHelper.setTo(mail.getTo());
+            mimeMessageHelper.setText(mail.getContent());
+
+            emailSender.send(mimeMessageHelper.getMimeMessage());
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+
+    }
+}
