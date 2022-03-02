5e8aeec11be0696116710109a3ed3464b23f754d
cphall <christianphall@gmail.com>
2022-03-02 00:38:19 -0500

    Made various changes to try and get google social to work

--- Project2/ProjectManagementAPI/src/main/java/revature/ProjectManagementAPI/security/oauth/WebSecurityConfig.java
+++ Project2/ProjectManagementAPI/src/main/java/revature/ProjectManagementAPI/security/oauth/WebSecurityConfig.java
@@ -5,10 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
 
     /**
      * Configures HTTPSecurity requests for specific URLs
@@ -19,23 +30,39 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/index.html", "/oauth/**").permitAll()
-                .antMatchers("/user/**").authenticated()
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                    .loginPage("/index.html")
-                    .usernameParameter("email")
-                    .permitAll()
-                    .defaultSuccessUrl("/")
+                .formLogin().permitAll()
+                .loginPage("/index.html")
+                .usernameParameter("email")
+                .passwordParameter("pass")
+                .defaultSuccessUrl("/list")
                 .and()
                 .oauth2Login()
-                    .loginPage("/index.html")
-                    .userInfoEndpoint().userService(oauthUserService)
-                    .and().successHandler(oAuth2LoginSuccessHandler);
-                /*.and()
-                .logout().permitAll()
+                .loginPage("/index.html")
+                .userInfoEndpoint()
+                .userService(oauthUserService)
                 .and()
-                .rememberMe().tokenRepository(persistentTokenRepository());*/
+                .successHandler(new AuthenticationSuccessHandler() {
+
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                        Authentication authentication) throws IOException, ServletException {
+                        System.out.println("AuthenticationSuccessHandler invoked");
+                        System.out.println("Authentication name: " + authentication.getName());
+                        OAuth2User oauthUser = (OAuth2User) authentication.getPrincipal();
+                        //TODO Implement
+                        //oauthUserService.processOAuthPostLogin(oauthUser.getEmail());
+
+                        response.sendRedirect("/list");
+                    }
+                })
+                //.defaultSuccessUrl("/list")
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/403")
+        ;
     }
 
     @Autowired
